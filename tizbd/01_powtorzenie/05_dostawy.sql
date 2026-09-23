6388c7f33230af22299d9a6cfdc1bf33d6aa3e4a
Jolanta Gryko <jgryko@gmail.com>
2026-09-23 13:34:53 +0200

    23.09

--- tizbd/01_powtorzenie/05_dostawy.sql
+++ tizbd/01_powtorzenie/05_dostawy.sql
@@ -116,15 +116,45 @@ WHERE cena = (
 
 -- 7. Dodaj  Ofertę produktu o kodzie 1 dostawcy "Skellington Supplies"  o cenie 7.
 
- 
+INSERT INTO Oferty
+(produkt, dostawca, cena)
+VALUES
+(1,(SELECT kod
+FROM Dostawcy
+WHERE nazwa = 'skellington supplies'), 7);
+
 
 -- 8. Podnieś cenę wszystkich produktów o 1
 
-
+UPDATE oferty
+SET CENA = CENA +1;
 -- 9. Producent  "Susan Calvin Corp." nie będzie dostarczał już wkrętów  (bolt). Usuń właściwy wiersz z tabeli Oferty.
+DELETE FROM Oferty
+WHERE dostawca = (
+    SELECT kod
+    FROM dostawcy
+    WHERE nazwa = 'Susan Calvin Crop.'
+)
+AND produkt = (
+    SELECT kod
+    FROM produkty
+    WHERE nazwa = 'bolt'
+);
 
 -- 10. Usuń z bazy dostawcę o kodzie "RBT"
+-- UPDATE oferty
+-- SET dostawca = NULL
+-- WHERE dostawca = 'RBT';
+DELETE FROM oferty
+WHERE dostawca = 'RBT';
+
 DELETE FROM Dostawcy
 WHERE kod = "RBT";
 
 -- 11. Usuwamy z oferty (tabela Oferty) nakrętki (nut). Właściwy kod produktu wyszukaj podzapytaniem.
+DELETE FROM Oferty
+WHERE produkt = (
+    SELECT kod
+    FROM produkty
+    WHERE nazwa = 'nut'
+);
